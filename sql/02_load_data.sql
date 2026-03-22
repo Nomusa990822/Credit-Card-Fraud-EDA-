db650ecf135cd0b59e81eed52472b3ac090a1eff
Nomusa Shongwe <shongwenomusa990822@gmail.com>
2026-03-22 04:30:43 +0200

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,14 +1,64 @@
-COPY fraud_train
-FROM '/path/to/train.csv'
+-- Load training data
+COPY fraud_train (
+    row_id,
+    trans_date_trans_time,
+    cc_num,
+    merchant,
+    category,
+    amt,
+    first_name,
+    last_name,
+    gender,
+    street,
+    city,
+    state,
+    zip,
+    lat,
+    long,
+    city_pop,
+    job,
+    dob,
+    trans_num,
+    unix_time,
+    merch_lat,
+    merch_long,
+    is_fraud
+)
+FROM '/absolute/path/to/data/train.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY fraud_test
-FROM '/path/to/test.csv'
+-- Load testing data
+COPY fraud_test (
+    row_id,
+    trans_date_trans_time,
+    cc_num,
+    merchant,
+    category,
+    amt,
+    first_name,
+    last_name,
+    gender,
+    street,
+    city,
+    state,
+    zip,
+    lat,
+    long,
+    city_pop,
+    job,
+    dob,
+    trans_num,
+    unix_time,
+    merch_lat,
+    merch_long,
+    is_fraud
+)
+FROM '/absolute/path/to/data/test.csv'
 DELIMITER ','
 CSV HEADER;
 
--- Combine Data
+-- Combine train and test
 CREATE TABLE fraud_all AS
 SELECT * FROM fraud_train
 UNION ALL
